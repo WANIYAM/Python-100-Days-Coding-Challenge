2c802e90aaddd3f46ddb392af41b5b243cb10882
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-04-09 19:59:17 +0500

    day 10

---
+++ day-10/main.py
@@ -0,0 +1,10 @@
+# 10.Reverse a number and a string.
+
+num = 12345
+reversed_num = int(str(num)[::-1])
+print(reversed_num) 
+text =" word"
+reversed_text = ((text)[::-1])
+print(reversed_text) 
+
+
