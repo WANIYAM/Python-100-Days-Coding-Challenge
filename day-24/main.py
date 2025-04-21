6863b124a2ccde56f62a39acafecd87bc3f3b592
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-04-21 13:03:16 +0500

    day 24

---
+++ day-24/main.py
@@ -0,0 +1,10 @@
+# 24 Write a function to reverse a string.
+
+def reverse_string(s):
+    return s[::-1]
+
+# Example usage:
+text = "hello"
+print("Reversed string:", reverse_string(text))
+text = "world"
+print("Reversed string:", reverse_string(text))
