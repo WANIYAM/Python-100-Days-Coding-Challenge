7624992528ed94432aa04c1213a29c7dd60228f9
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-05-04 12:16:22 +0500

    day 35

---
+++ day-35/main.py
@@ -0,0 +1,10 @@
+# 35.Write a program to convert a list of tuples into a dictionary.
+
+# List of tuples
+tuple_list = [("a", 1), ("b", 2), ("c", 3)]
+
+# Convert to dictionary
+result_dict = dict(tuple_list)
+
+# Print the dictionary
+print("Converted dictionary:", result_dict)
