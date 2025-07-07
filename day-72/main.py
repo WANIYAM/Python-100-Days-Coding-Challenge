91254da03482b0980d76ba5dadd6474da5b853f2
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-07-07 13:02:35 +0500

    day 72

---
+++ day-72/main.py
@@ -0,0 +1,18 @@
+# 72.Use built-in math module (factorial, sqrt, gcd).
+import math
+
+# Using factorial()
+num = 5
+fact = math.factorial(num)
+print(f"Factorial of {num} is {fact}")
+
+# Using sqrt()
+value = 16
+square_root = math.sqrt(value)
+print(f"Square root of {value} is {square_root}")
+
+# Using gcd()
+a = 36
+b = 60
+common_divisor = math.gcd(a, b)
+print(f"GCD of {a} and {b} is {common_divisor}")
