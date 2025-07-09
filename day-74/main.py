3b08c5073a3662e36b7a39e85429ea4214c6d846
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-07-09 13:39:23 +0500

    day 74

---
+++ day-74/main.py
@@ -0,0 +1,9 @@
+# 74.Use datetime to print current date and time.
+
+import datetime
+
+# Get current date and time
+current_datetime = datetime.datetime.now()
+
+# Print current date and time
+print("Current date and time:", current_datetime)
