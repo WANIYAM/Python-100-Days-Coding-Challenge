06c99c5aa7cc8b0caf379cf22736703324adfb53
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-07-11 13:02:22 +0500

    day 76

---
+++ day-76/main.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+# Define two dates
+date1 = datetime(2025, 7, 11)
+date2 = datetime(2024, 12, 25)
+
+# Calculate the difference
+difference = date1 - date2
+
+# Print the difference in days
+print("Difference:", difference.days, "days")
