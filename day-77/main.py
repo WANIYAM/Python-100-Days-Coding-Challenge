7e813d534eee0c37e3f570e974cbeb144a2ccc72
WANIYAM <164039644+WANIYAM@users.noreply.github.com>
2025-07-12 12:00:07 +0500

    day 77

---
+++ day-77/main.py
@@ -0,0 +1,9 @@
+# 77.Use calendar to display a month and year.
+
+import calendar
+
+year = 2025
+month = 7
+
+# Display the calendar for the specified month and year
+print(calendar.month(year, month))
